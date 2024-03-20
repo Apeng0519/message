146e791405db38eb1239ef8afaccce5b308844cf
Apeng0519 <152376971+Apeng0519@users.noreply.github.com>
2024-03-19 21:32:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, see you on May 17th!";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://giphy.com/gifs/pudgypenguins-sending-love-hugs-hearts-0EJWybud11odVuLrPi";
 });
 
 noBtn.addEventListener("mouseover", () => {
